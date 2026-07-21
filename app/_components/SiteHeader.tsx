e175435ea0f8f872963cb97381624463aa6a0aad
Jblessed3 <jobethvpacheco@gmail.com>
2026-07-21 10:18:56 -0400

    Prepared the header for the new black editorial wordmark

--- app/_components/SiteHeader.tsx
+++ app/_components/SiteHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Menu, X } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
