a8c874b3a6fb497d44a7407f1cca9b414416f2b1
Jblessed3 <jobethvpacheco@gmail.com>
2026-08-18 23:38:55 -0400

    Updated Day 3 devotional video

--- app/unashamed-devotionals/page.tsx
+++ app/unashamed-devotionals/page.tsx
@@ -4,7 +4,7 @@ import { SiteFooter } from "../_components/SiteFooter";
 import { SiteHeader } from "../_components/SiteHeader";
 
 const devotionalVideoUrl =
-  "https://o3hectmev11nr3rl.public.blob.vercel-storage.com/church-uploads/9tk8EKBJqDgoS5nPZ-qpSBuY561vd7PJ/54025-iRnGpHxtpqiRMq3qb32E3ZCY4QkQ4q.mp4";
+  "https://o3hectmev11nr3rl.public.blob.vercel-storage.com/church-uploads/7hufTsj5mHzvDVIWx5Jp-eWPUoez_j1r/54119-YLMn7NWJnX6El6BAB8efk9wJmJ16up.mp4";
 
 export default function UnashamedDevotionalsPage() {
   return (
